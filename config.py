80e5d36a165adf086d98b0f14b602a28a00b901c
khaled mansour <hsohcl71@gmail.com>
2026-09-04 12:43:18 +0300

    chore: setup project environment, config, and demo data

---
+++ config.py
@@ -0,0 +1,5 @@
+BAUD_RATE=9600
+SMOOTHING_WINDOW=3
+MIN_RELATIVE_SEPARATION=0.05
+COMPARISON_METHOD="hybrid"  # mse | correlation | hybrid
+CHANNELS=["resistance_reference","sensitivity_reference","test_1","test_2","test_3","test_4","test_5","test_6"]
